9654fc6dcd3a2247a97993785a5e0262baef944c
John <paulmuresh12@gmail.com>
2023-07-24 17:43:43 -0700

    reset

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h> // For atoi function
+#include <string.h>
 
-#include "main.h"
+// Function declaration for parseNumber
+int parseNumber(const char *s);
 
-/**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(char *s)
+int main(void)
 {
-	int i, d, n, len, f, digit;
+    const char *input = "12345";
+    int result = parseNumber(input);
+    printf("Parsed number: %d\n", result);
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+    return 0;
+}
 
-	while (s[len] != '\0')
-		len++;
+// The parseNumber function definition remains unchanged.
+int parseNumber(const char *s)
+{
+    int i, d, n, len, f, digit;
 
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
+    i = 0;
+    d = 0;
+    n = 0;
+    len = 0;
+    f = 0;
+    digit = 0;
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
+    while (s[len] != '\0')
+        len++;
 
-	if (f == 0)
-		return (0);
+    while (i < len && f == 0)
+    {
+        if (s[i] == '-')
+            ++d;
 
-	return (n);
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            digit = s[i] - '0';
+            if (d % 2)
+                digit = -digit;
+            n = n * 10 + digit;
+            f = 1;
+            if (s[i + 1] < '0' || s[i + 1] > '9')
+                break;
+            f = 0;
+        }
+        i++;
+    }
+
+    if (f == 0)
+        return 0;
+
+    return n;
 }
